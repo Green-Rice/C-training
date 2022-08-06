32e5b24ababa8dcd8d8d712057334bf39a3e1c2f
Green rice <alexeyerokhin_ck@mail.ru>
2022-08-06 16:21:58 +0300

    First lesson homework completed

---
+++ Homework/Lesson1HW task1/Lesson1HW task3/task3.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+int num = Prompt("Введите число для проверки >");
+if (num % 2 == 0)
+{
+    System.Console.Write("Число четное");
+}
+else
+{
+    System.Console.Write("Число  нечетное");
+}
---
+++ Homework/Lesson1HW task1/Lesson1HW task4/Task4.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+int Promt(string message)
+{
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int num1 = Promt("Введите число : ");
+
+for (int i = 2; i <= num1; i=i+2)
+    {
+        System.Console.Write(i+" ");
+    }
