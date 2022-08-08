a4f8c666d508430ee18f7a47eb86ad1d8dbb2f8d
Green rice <alexeyerokhin_ck@mail.ru>
2022-08-08 13:24:47 +0300

    task from the second lecture

---
+++ Lesson/Lesson2/Program.cs
@@ -0,0 +1,64 @@
+﻿// int[] arrya = { 15, 12, 23, 54, 15, 4, 57, 38, 29 };
+
+// int n = arrya.Length;
+// int find = 38;
+// int index = 0;
+
+// while (index < n)
+// {
+
+//     if (arrya[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;
+//     }
+//     index++;
+// }
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
