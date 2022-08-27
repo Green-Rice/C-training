738c9ef3913e9a6b5bf7c636738fad8f9188f4b5
Green rice <alexeyerokhin_ck@mail.ru>
2022-08-27 15:00:55 +0300

    Finish 6 HW

---
+++ Homework/Lesson6HM/Task1/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+
+int[] InputArray(int num)
+{
+    int[] array = new int[num];
+    int i = 0;
+    while (i < num)
+    {
+        Console.Write("Введите число ");
+        array[i] = int.Parse(Console.ReadLine());
+        i++;
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}  ");
+    }
+}
+
+
+int CountingPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+//____________________________________________________________
+
+
+int[] newArray = InputArray(Prompt("Введите размер массива"));
+PrintArray(newArray);
+System.Console.WriteLine($"Колличество чисел больше 0; {CountingPositiveNumbers(newArray)}");
---
+++ Homework/Lesson6HM/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+
+void CalculationIntersectionPoints(double x1, double x2, double y1, double y2)
+{
+    if (x1 == x2)
+    {
+        if (x1 == x2 && y1 == y2)
+        {
+            System.Console.WriteLine("Линии совпадают!");
+        }
+        else
+        {
+            System.Console.WriteLine("Пересечения линий нет!");
+        }
+    }
+    else
+    {
+        double a = (-x2 + x1) / (-y1 + y2);
+        double b = y2 * a + x2;
+        Console.WriteLine($"Точка пересечения: ({a}; {b})");
+    }
+}
+
+double x1 = Prompt("Введите кординату х1 :");
+double y1 = Prompt("Введите кординату y1 :");
+double x2 = Prompt("Введите кординату x2 :");
+double y2 = Prompt("Введите кординату y2 :");
+CalculationIntersectionPoints(x1, x2, y1, y2);
