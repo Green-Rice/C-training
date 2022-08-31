64e4612f4d5dfeb29d743cf48e08a748e54b7a10
Green rice <alexeyerokhin_ck@mail.ru>
2022-08-31 12:28:56 +0300

    Finish HW 7

---
+++ Homework/Lesson7HW/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] FillArray(int line, int columns, int min, int max)
+{
+    double[,] array = new double[line, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max)/10.0;
+        }
+    }
+    return array;
+}
+
+double[,] array = FillArray(Prompt("Введите колличество строк :"), Prompt("Введите колличество столбиков :"), -100, 100);
+PrintArray(array);
---
+++ Homework/Lesson7HW/Task2/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*
+Задача 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int[,] FillArray(int line, int columns, int min, int max)
+{
+    int[,] array = new int[line, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool IndexCheck(int[,] array, int line, int column)
+{
+    bool result = true;
+    if (array.GetLength(0) <= line || array.GetLength(1) <= column)
+    {
+        result = false;
+    }
+    return result;
+}
+
+void FindingElementArray(int[,] array, int line, int column)
+{
+    if (IndexCheck(array, line, column))
+    {
+        System.Console.WriteLine(array[line, column]);
+    }
+    else
+    {
+        System.Console.WriteLine("Элемента нет.");
+    }
+}
+
+int [,]array = FillArray(4,5,1,100);
+PrintArray(array);
+FindingElementArray(array, Prompt("Введите номер строки =>"), Prompt("Введите номер столбца =>"));
